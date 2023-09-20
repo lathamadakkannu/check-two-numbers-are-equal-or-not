20acd291b56fe8c74c2ee7a2082654ddd8aa8a47
lathamadakkannu <lathamadakkannu@gmail.com>
2023-09-20 11:30:55 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,9 @@
+var a=prompt("enter the first value");
+var b=prompt("enter the second value");
+
+if(a==b){
+    document.write("numbers are equal");
+}
+else{
+    document.write("numbers are not equal");
+}
